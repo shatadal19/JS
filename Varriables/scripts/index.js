4fc57a0bda4d2013280e83acf74c8a2c832ce663
unknown <shatadalb19@gmail.com>
2025-09-25 11:35:34 +0600

    varriable v1

---
+++ Varriables/scripts/index.js
@@ -0,0 +1,11 @@
+// let welcomemassage = "Hello, welcome to the JavaScript world!";
+// console.log(welcomemassage);
+// console.log("This is my first JavaScript code.");
+
+
+let bagPrice = (15599-(10/100*155990))-272;
+let jeansPrice = (2499 - (41/100*2499))-29;
+let bagVale = Math.round((jeansPrice + bagPrice + 25 + 20)*118/100)
+
+let bagSummary = `your final cart valu is ${bagVale}`
+console.log(jeansPrice)
