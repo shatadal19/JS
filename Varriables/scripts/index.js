9ad2b64db1e02a7148a8654d3c95bb58d6f48916
unknown <shatadalb19@gmail.com>
2025-09-25 11:59:53 +0600

    const

--- Varriables/scripts/index.js
+++ Varriables/scripts/index.js
@@ -3,6 +3,15 @@
 
 // console.log(a+b);
 
-let money = 1;
-money +=4;
-console.log(money);
+// let money = 1;
+// money +=4;
+// console.log(money);
+
+
+let a = 6;
+a = 8;
+// you can update your value by using let
+
+var b = 5;
+b = 7;
+// you can't update your value
