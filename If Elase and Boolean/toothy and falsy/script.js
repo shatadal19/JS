1e30da1cf20cd07486ea4f1715d69716213ace11
unknown <shatadalb19@gmail.com>
2025-09-27 13:49:12 +0600

    null

--- If Elase and Boolean/toothy and falsy/script.js
+++ If Elase and Boolean/toothy and falsy/script.js
@@ -6,7 +6,16 @@
 // else{
 //     console.log("Condition is false");
 // }
-let age;
+// let age;
+
+// if(age){
+//     console.log("Condition is true");   
+// }
+// else{
+//     console.log("Condition is false");
+// }
+let age = null;
+console.log(age);
 
 if(age){
     console.log("Condition is true");   
