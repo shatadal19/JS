680423fe6e6de13055a9c9e80ec5b54e6a0c6fdc
unknown <shatadalb19@gmail.com>
2025-09-27 13:55:10 +0600

    Ternary Operator

--- If Elase and Boolean/If Alternatives/script.js
+++ If Elase and Boolean/If Alternatives/script.js
@@ -1,9 +1,15 @@
+// let age = 14;
+// let result;
+// if(age >= 18){
+//     result = "You are an adult";
+// }  
+// else{
+//     result = "You are a minor";
+// }
+// console.log(result);
+
+// Ternary Operator
+
 let age = 14;
-let result;
-if(age >= 18){
-    result = "You are an adult";
-}  
-else{
-    result = "You are a minor";
-}
+let result = (age >= 18) ? "You are an adult" : "You are a minor";
 console.log(result);
