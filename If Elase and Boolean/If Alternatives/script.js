8d1904ac0c6806cf5a93e537b583837d49b24313
unknown <shatadalb19@gmail.com>
2025-09-27 13:57:59 +0600

    guard operator

--- If Elase and Boolean/If Alternatives/script.js
+++ If Elase and Boolean/If Alternatives/script.js
@@ -10,6 +10,15 @@
 
 // Ternary Operator
 
-let age = 14;
-let result = (age >= 18) ? "You are an adult" : "You are a minor";
-console.log(result);
+// let age = 14;
+// let result = (age >= 18) ? "You are an adult" : "You are a minor";
+// console.log(result);
+
+
+// guard operator
+
+
+let age;
+let finalAge = age || 18;
+console.log(age);
+console.log(finalAge);
